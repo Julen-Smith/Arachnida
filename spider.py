46554a60b4037e7461c7e53f6c8549cdfe3d3b56
Julen Smith <jsmith@c4r4s4.42urduliz.com>
2022-07-22 17:55:59 +0200

    Recursividad funcional, flags y apaños sin hacer, extensiones sin analizar

---
+++ spider.py
@@ -0,0 +1,77 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    spider.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jsmith <jsmith@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/07/22 16:30:19 by jsmith            #+#    #+#              #
+#    Updated: 2022/07/22 17:55:14 by jsmith           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sys
+from bs4 import *
+import requests
+import os
+from PIL import Image
+
+"""
+El programa spider permitirá extraer todas las imágenes de un sitio web, de manera
+recursiva, proporcionando una url como parámetro. Gestionarás las siguientes opciones
+del programa
+"""
+# Opción -r : descarga de forma recursiva las imágenes en una URL recibida como parámetro.
+# Opción -r -l [N] : indica el nivel profundidad máximo de la descarga recursiva. En caso de no indicarse, será 5.
+# Opción -p [PATH] : indica la ruta donde se guardarán los archivos descargados. En caso de no indicarse, se utilizará ./data/.
+
+jpg = ".jpg"
+jpeg = ".jpeg"
+png = ".png"
+gif = ".gif"
+bmp = ".bmp"
+
+def recursive_donwload(images, nbr , i):
+	print("Entro aqui por "+ str(i) + " vez")
+	if (i != nbr) :
+		recursive_donwload(images,nbr,i + 1)
+	print("Se ejecuta el launch numero " + str(i))
+	try:
+		try:
+			image_link = images[i]["data-srcset"]
+		except:
+			try:
+				image_link = images[i]["data-src"]
+			except:
+				try:
+					image_link = images[i]["data-fallback-src"]
+				except:
+					try:
+						image_link = images[i]["src"]
+					except:
+						pass
+		del images[i]
+		print("Numero de imagenes " + str(len(images)));
+		#headers te dice el tipo que es 
+		r = requests.get(image_link).headers
+		dict.fromkeys(r)
+		print(r['Content-Type:'])
+		try:
+			r = str(r, 'utf-8')
+		except UnicodeDecodeError: 
+			#folder_name = "."
+			with open(".", "wb+") as f: 
+				f.write(r)
+	except:
+		pass
+
+
+if __name__ == "__main__":
+	if len(sys.argv ) > 3 :
+		print ("Arg error")
+	print("Request will be send to : " + sys.argv[2])
+	req = requests.get(sys.argv[2])
+	soup = BeautifulSoup (req.text, 'html.parser')
+	images = soup.findAll('img')
+	print("Succesfull " + str(len(images)) + " image/s found!")
+	recursive_donwload(images,5,1)
